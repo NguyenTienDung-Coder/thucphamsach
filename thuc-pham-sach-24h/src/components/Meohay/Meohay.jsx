1b9cb93082952148c9e9682b00810b1c09b47151
Dung <nguyentiendung342000@gmail.com>
2022-06-14 16:30:59 +0700

    up code

---
+++ thuc-pham-sach-24h/src/components/Meohay/Meohay.jsx
@@ -0,0 +1,74 @@
+import Category from "../category";
+import './Meohay.css';
+
+function Meohay () {
+    return(
+        <div className="page-news">
+            <Category name='Mẹo hay'></Category>
+            <div className="all-new">
+                <div className="all-categories">
+                    <div className="categories-one">
+                        <span className="span-categories">Chuyên mục</span>
+                        <li className="li-categories-t" ><a href="#">Mẹo hay</a></li>
+                        <li className="li-categories-t" ><a href="#">Món ngon</a></li>
+                        <li className="li-categories" ><a href="#">Tin tứ</a>c</li>
+                    </div>
+                    <div className="categories-one-t">
+                        <span className="span-categories">bài mới</span>
+                        <li className="li-categories-t" ><a href="#">Cách làm tôm một nắng đơn giản ngọt dai để ăn Tết</a></li>
+                        <li className="li-categories-t" ><a href="#">Cách làm khô cá đù thơm ngon, lạ vị dễ làm tại nhà</a></li>
+                        <li className="li-categories-t" ><a href="#">Cách làm cà na đập muối ớt, thơm ngon hấp dẫn, cực đơn giản</a>c</li>
+                        <li className="li-categories-t" ><a href="#">Hướng dẫn chế biến cá hồi cho bé ăn cơm hoặc ăn dặm</a></li>
+                        <li className="li-categories" ><a href="#">Cách sơ chế rau củ quả đúng cách, an toàn cho sức khỏe</a></li>
+
+
+                    </div>
+                </div>
+
+
+
+                <div className="thong-tin">
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2021/07/ca-hoi-400x225.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2021/07/cach-so-che-rau-cu-qua-dung-cach-an-toan-cho-suc-khoe-1627172436-400x300.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2021/07/nen-mua-ca-chua-thuong-hay-ca-chua-bi-1627171872-400x268.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2021/07/cach-bao-quan-rau-cu-tuoi-lau-ma-khong-can-tu-lanh-1626398712-1-400x260.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2021/07/cach-bao-quan-rau-trong-tu-lanh-ca-tuan-ma-van-tuoi-ngon-1626350967-400x267.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2020/08/bach-tuoc-nuong-va-5-cach-che-bien-thom-ngon-de-ban-don-mua-mua-ve-400x267.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                    <div className="news-one">
+                        <img className="img-news" src="https://cuahangtienloi24h.com/wp-content/uploads/2020/08/muc-ong-chien-gion-cach-lam-ngon-dung-chuan-nha-hang-cuc-de-400x275.jpg" alt="" />
+                        <h5 className="inf-h5">Tác dụng của mồng tơi – Ăn ngon sống khoẻ cùng mồng tơi</h5>
+                        <p className="inf-p">Rau mồng tơi&nbsp;là món ăn dân giã nhưng lại là món ăn bài thuốc quen...</p>
+                    </div>
+                </div>
+            </div>
+        <div className="gap"></div>
+        </div>
+    )
+}
+
+
+export default Meohay;
